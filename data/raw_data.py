0c3022c31c0f95d7b5b9420612db90b21db12e44
shandilya1998 <shandilya.shreyas@gmail.com>
2019-11-08 17:22:32 +0530

    commit apply logger

--- data/raw_data.py
+++ data/raw_data.py
@@ -33,7 +33,7 @@ class Data():
         """
             This function returns all the unique word keys in wiktionary dumps
             output - (numpy.ndarray, int)
-        """t
+        """
         keys = self.sample[0].unique()
         num_keys = len(keys)
         return (keys, num_keys) 
@@ -77,13 +77,41 @@ class Data():
 
     def compile_dictionary(self):
         for i in tqdm(range(1,self.data.shape[1])):
-            self.data.iloc[:,i] = self.data.iloc[:,i].apply(self.definition)
+            self.data.iloc[:,i] = self.logged_apply(self.data.iloc[:,i], self.definition)
         dictionary = pd.DataFrame(columns = ['word', 'definition'])
         for i in tqdm(range(len(self.data))):
             definitions = self.data.iloc[i, 1:]
             definitions = definitions.dropna().values 
             dictionary = dictionary.append(pd.DataFrame([[self.data.iloc[i, 0], definitions]], columns = ['word', 'definition']))
         return dictionary
+
+    def logged_apply(self, g, func, *args, **kwargs):
+        """
+            g - dataframe
+            func - function to apply to the dataframe
+            *args, **kwargs are the arguments to func
+            The method applies the function to all the elements of the dataframe and shows progress
+        """
+        step_percentage = 100. / len(g)
+        import sys
+        sys.stdout.write('apply progress:   0%')
+        sys.stdout.flush()
+
+        def logging_decorator(func):
+            def wrapper(*args, **kwargs):
+                progress = wrapper.count * step_percentage
+                sys.stdout.write('\033[D \033[D' * 4 + format(progress, '3.0f') + '%')
+                sys.stdout.flush()
+                wrapper.count += 1
+                return func(*args, **kwargs)
+            wrapper.count = 0
+            return wrapper
+
+        logged_func = logging_decorator(func)
+        res = g.apply(logged_func, *args, **kwargs)
+        sys.stdout.write('\033[D \033[D' * 4 + format(100., '3.0f') + '%' + '\n')
+        sys.stdout.flush()
+        return res
 
 data = Data()
 #data.remove_en_NaN()
