fb9615aaf400b4ef871a88f1b2784772a61c4f66
shandilya1998 <shandilya.shreyas@gmail.com>
2019-11-21 01:55:08 +0530

    run this to construct graph and get pagerank

--- run_construct.py
+++ run_construct.py
@@ -1,9 +1,12 @@
+import os
+os.chdir('data/')
+os.listdir()
 from data.graph import df2, graph
 #from data.preporcess import prepocess
 
 m_graph = graph(df2).m_construct_graph()
 #graph = graph(df2).construct_graph()
-
+os.chdir('..')
 import pickle
 file = 'data/m_graph.pickle'
 pkl = open(file)
