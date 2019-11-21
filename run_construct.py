46c33f39b479d2437ff94c4a7240e1b2600d70ea
shandilya1998 <shandilya.shreyas@gmail.com>
2019-11-21 23:51:52 +0530

    run_construct commit

--- run_construct.py
+++ run_construct.py
@@ -1,19 +1,7 @@
-import os
-os.chdir('data/')
-os.listdir()
-from data.graph import df2, graph
-#from data.preporcess import prepocess
+from data.raw_data import data
+from data.graph import m_graph
+from source.pagerank import pagerank
+from source.scc import scc
 
-m_graph = graph(df2).m_construct_graph()
-#graph = graph(df2).construct_graph()
-os.chdir('..')
-import pickle
-file = 'data/m_graph.pickle'
-pkl = open(file)
-pickle.dump(m_graph, pkl)
-pkl.close()
-
-from source.pagerank import test1
-print(tests.pagerank)
 
 
